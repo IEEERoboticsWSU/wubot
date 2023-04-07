c78c4d9f2e95c6d0a00d31f749f73e9c8f98c1ae
Jason Wibowo <jtgalways@gmail.com>
2023-04-07 10:15:32 -0500

    To test Must add launch files later

--- launch/lidar.launch.py
+++ launch/lidar.launch.py
@@ -12,7 +12,7 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/serial/by-path/platform-3610000.xhci-usb-0:2.2:1.0-port0'
+                'serial_port': '/dev/serial/by-path/platform-3610000.xhci-usb-0:2.3:1.0-port0',
                 'frame_id': 'laser_frame',
                 'angle_compensate': True,
                 'scan_mode': 'Standard'
